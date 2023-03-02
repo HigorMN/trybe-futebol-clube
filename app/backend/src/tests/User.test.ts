63ab4f20b5a8faa027777b1b9b3c4af19c2e8a67
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-03-02 09:17:11 -0300

    test: testando a rota GET /login/role

--- app/backend/src/tests/User.test.ts
+++ app/backend/src/tests/User.test.ts
@@ -1,6 +1,7 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
 import * as bcrypt from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 
@@ -13,7 +14,8 @@ chai.use(chaiHttp);
 
 const { expect, request } = chai;
 
-describe('Teste da aplicação: login', () => {
+const JWT_SECRET = process.env.JWT_SECRET || 'secret' as string;
+describe('Teste da aplicação: POST /login', () => {
   beforeEach(sinon.restore);
 
   const user = new UserModel({
@@ -48,6 +50,18 @@ describe('Teste da aplicação: login', () => {
     expect(result.body).to.deep.equal({ message: 'All fields must be filled'})
   });
 
+  it('Deve retornar um erro caso o formato do email for inválido', async () => {
+    const body = { email: 'emailinválido', password: '123456'}
+
+    sinon.stub(Model, 'findOne').resolves(user);
+    sinon.stub(bcrypt, 'compareSync').resolves(true);
+
+    const result = await request(app).post('/login').send(body);
+
+    expect(result.status).to.be.equal(401);
+    expect(result.body).to.deep.equal({ message: 'Invalid email or password'})
+  });
+
   it('Deve retornar um erro caso não existir password', async () => {
     const body = { email: 'higor.maranhao2000@gmail.com', password: ''}
 
@@ -61,7 +75,7 @@ describe('Teste da aplicação: login', () => {
   });
 
   it('Deve retornar um erro caso email incorreto', async () => {
-    const body = { email: 'emailIncorreto', password: '123456'}
+    const body = { email: 'higor@gmail.com', password: '123456'}
 
     sinon.stub(Model, 'findOne').resolves(null);
 
@@ -72,7 +86,7 @@ describe('Teste da aplicação: login', () => {
   });
 
   it('Deve retornar um erro caso password incorreto', async () => {
-    const body = { email: 'higor.maranhao2000@gmail.com', password: '12'}
+    const body = { email: 'higor.maranhao2000@gmail.com', password: '12345678'}
 
     sinon.stub(Model, 'findOne').resolves(user);
 
@@ -81,5 +95,41 @@ describe('Teste da aplicação: login', () => {
     expect(result.status).to.be.equal(401);
     expect(result.body).to.deep.equal({ message: 'Invalid email or password'})
   });
-
 });
+
+describe('Teste da aplicação: GET /login/role', () => {
+  beforeEach(sinon.restore);
+
+  const user = { 
+    id: 1,
+    username: 'Higor Maranhão',
+    role: 'admin',
+    email: 'higor.maranhao2000@gmail.com',
+   };
+
+  it('Deve retornar a role do usuário autenticado', async () => {
+    const token = jwt.sign(user, JWT_SECRET);
+    const result = await request(app).get('/login/role').set('Authorization', token);
+
+    expect(result.status).to.deep.equal(200);
+    expect(result.body).to.deep.equal({ role: 'admin'})
+  });
+
+  it('Deve retornar um erro 401 se o token estiver ausente', async () => {
+    const result = await request(app).get('/login/role')
+
+    expect(result.status).to.deep.equal(401);
+    expect(result.body).to.deep.equal({ message: 'Token not found'})
+  });
+
+  it('Deve retornar um erro 401 se o token for inválido', async () => {
+    const token = 'token_inválido';
+
+    const result = await request(app)
+      .get('/login/role')
+      .set('Authorization', token);
+
+    expect(result.status).to.deep.equal(401);
+    expect(result.body).to.deep.equal({ message: 'Token must be a valid token'})
+  });
+})
